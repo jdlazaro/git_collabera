cccd0ef944f083699a96b7791092bf9090bd4b51
John Paul Lazaro <johnpauldelacruzlazaro@gmail.com>
2020-10-13 10:19:40 +0800

    Updates

---
+++ precache-manifest.9f5fa42af172232679c79f3e69186151.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc8c50949f15c4399a998cd02da1e51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3162db202caf9e71cae8",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f970cc4c3fcef2606e1f",
+    "url": "/static/css/main.f18b5526.chunk.css"
+  },
+  {
+    "revision": "3162db202caf9e71cae8",
+    "url": "/static/js/2.741ad628.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.741ad628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f970cc4c3fcef2606e1f",
+    "url": "/static/js/main.aee0edfe.chunk.js"
+  },
+  {
+    "revision": "b550b664bb71128565b2",
+    "url": "/static/js/runtime-main.150fb853.js"
+  }
+]);
